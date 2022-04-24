4192c5d8d76e39a1d45ce4d57c19b76f75a86cc2
Bradmin82 <bweldy82@gmail.com>
2022-04-24 12:33:14 -0700

    functional

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function getTodos() {
   //   .catch(err => console.error(err));
 
   axios
-    .get('https://jsonplaceholder.typicode.com/todos?_limit=5', {
+    .get('https://jsonplaceholder.typicode.com/todos?_limit=5', { params: { _limit: 10}}, {
       timeout: 5000
     })
     .then(res => showOutput(res))
